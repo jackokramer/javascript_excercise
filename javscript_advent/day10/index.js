d25f68de8aac5f49ddada1f6281a13c105f57a65
jackokramer <jackokramer@gmail.com>
2021-01-13 21:09:24 -0800

    dice game

--- javscript_advent/day10/index.js
+++ javscript_advent/day10/index.js
@@ -12,10 +12,54 @@ event listeners, Math.random()
 */
 
 // Write your code here 👇
+const dice = document.querySelector('.dice')
+const allDots = document.querySelectorAll('.dice div')
+//console.log(allDots)
 
 function diceRoll(spin){
-    
+    let randNumber = 1+ Math.floor(Math.random() * 6);   
+    console.log(randNumber); 
+
+    allDots.forEach(dot => dot.class.remove(...dot.classList))
+
+     if (randomNumber === 1) {
+      allDots[4].classList.add('dot')
+    }
+      
+  if (randomNumber === 2) {
+      allDots[0].classList.add('dot')
+      allDots[8].classList.add('dot')
+  }
+  if (randomNumber === 3) {
+      allDots[0].classList.add('dot')
+      allDots[4].classList.add('dot')
+      allDots[8].classList.add('dot')
+  }
+  if (randomNumber === 4) {
+      allDots[0].classList.add('dot')
+      allDots[2].classList.add('dot')
+      allDots[6].classList.add('dot')
+      allDots[8].classList.add('dot')
+  }
+  if (randomNumber === 5) {
+      allDots[0].classList.add('dot')
+      allDots[2].classList.add('dot')
+      allDots[4].classList.add('dot')
+      allDots[6].classList.add('dot')
+      allDots[8].classList.add('dot')
+  }
+
+    if(randNumber === 6){
+        allDots[0].classList.add('dot');
+        allDots[1].classList.add('dot');
+        allDots[2].classList.add('dot');
+        allDots[3].classList.add('dot');
+        allDots[4].classList.add('dot');
+        allDots[5].classList.add('dot');
+        allDots[6].classList.add('dot');
+    }
 }
+dice.addEventListener('click',diceRoll)
 /*
 
 DETAILED INSTRUCTIONS
