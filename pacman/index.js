b48ee71f0b6e32727727669a72a673d199b289b0
jackokramer <jackokramer@gmail.com>
2021-04-14 17:10:28 -0700

    pixel spacing pacman

--- pacman/index.js
+++ pacman/index.js
@@ -3,6 +3,7 @@
 1) Have t\JS display the world of bricks and couns
 2) ace the pacman move up and down
 */
+window.addEventListener('load', displayWorld, false);
 
 let world  = [
     [2,2,2,2,2,2,2,2,2,2],
@@ -19,18 +20,18 @@ let world  = [
 function displayWorld(){
     let output = '';
     for(let x = 0; x< world.length; x++){
-        output +="\n<div class='row'>";
+        output +="\n<div class='row'>\row";
         for (let z = 0; z< world[x]; z++){
             if(world[x][z]==2)
-                output += "\n\t<div class='brick'></div>"
+                output += "<div class='brick'></div>"
              else if(world[x][z]==1)
-                output += "\n\t<div class='coin'></div>"
+                output += "<div class='coin'></div>"
             if(world[x][z]==0)
-                output += "\n\t<div class='empty'></div>"
+                output += "<div class='empty'></div>"
         }
         output +="\n</div>";
     }
-    console.log(output);
+    //console.log(output);
     document.getElementById('world').innerHTML = output;
     
 }//iterate through world
