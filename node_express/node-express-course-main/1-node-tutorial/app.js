b40e4eb2af2a9e7a7b4be9edd7828dac92ce4b37
jackokramer <jackokramer@gmail.com>
2021-04-23 12:17:43 -0700

    start of it

---
+++ node_express/node-express-course-main/1-node-tutorial/app.js
@@ -0,0 +1,9 @@
+//global - NO WINDOW!!!!!!!! -there is no browser
+
+//__dirname     - path to the current directory
+//__filename    - file name
+// require      - function to use module (CommonJS)
+// module       - info about the current moule(file)
+// process      - infor about env where the program is being excecuted.
+
+console.log('Welcome to Node Tutorial')
