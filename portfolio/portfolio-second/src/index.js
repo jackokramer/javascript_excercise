7d61df348b208d40c01884ea7d40bf4ffb4c58dc
jackokramer <jackokramer@gmail.com>
2021-05-10 23:02:28 -0700

    portfolio-2 semi-complete. few media query errors

---
+++ portfolio/portfolio-second/src/index.js
@@ -0,0 +1,8 @@
+const hamButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburger.addEventListener('click', toggleButton)
