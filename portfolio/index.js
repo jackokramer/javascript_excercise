9a41b1b3ce15ea06d8f3d37a2567f23652c62115
jackokramer <jackokramer@gmail.com>
2021-05-26 19:18:44 -0700

    added a db to the portfolio

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,22 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 
+//MongDB
+/*
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+const db = client.db('user info')
+
+mongo.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client)=>{
+    if(err){
+        console.err(err)
+        return
+    }
+})
+*/
 
 navToggle.addEventListener('click', ()=>{
     document.body.classList.toggle('nav-open')
