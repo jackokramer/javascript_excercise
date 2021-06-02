2930a880c94f9c620831eca508f99cad44ae8c8a
jackokramer <jackokramer@gmail.com>
2021-06-02 09:49:02 -0700

    bcrypt and redis added for the twitter app

--- algos/basicII.js
+++ algos/basicII.js
@@ -92,12 +92,12 @@ function incrementInSec(arr){
 
 //Previous Lengths - You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
 
-function prevLength(arr){
+function previousLengths(arr){
     for(let x = arr.length - 1; x >= 0; x--){
-         arr[x] = arr[x-1].length
-        }
-        return arr
+        arr[x] = arr[x-1].length
     }
+    return arr;
+}
 
     console.log(prevLength(["hello", "dojo", "awesome"]))
 
--- flavio-js/twitter-redis/app.js
+++ flavio-js/twitter-redis/app.js
@@ -1,8 +1,44 @@
 const express = require('express')
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
 const path = require('path')
+
+//Redis feature
+const redis = require('redis')
+const client = redis.createClient()
+
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req,res) => res.render("index"));
 app.listen(3000, ()=> console.log('Sever ready'))
+
+app.post("/", (req, res) =>{
+    const {username, password} = req.body
+
+    //user name exists check if
+    client.hget("users", username, (err,userid)=>{
+        if(!userid){
+            //if the user does not exist, signup procedure will
+            client.incr('userid', async(err, userid)=>{
+                client.hset('users', username, userid)
+                const hash = await bcrypt.hash(password, saltRounds)
+                client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+            })
+        } else {
+            //user exists start login procedure
+        }
+    })
+    //username password are not entered fire this
+    if(!username || !password){
+        res.render('error', {
+            message: 'Please set a password and a username'
+        })
+        return
+    }
+    console.log(req.body, username, password)
+    res.end()
+})
