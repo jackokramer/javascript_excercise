20c919ed008a876f1c0b0fe82a0e5196e0c81bdc
jackokramer <jackokramer@gmail.com>
2021-06-16 14:38:12 -0500

    react app github-api

--- flavio-js/github-api-react/react-github-users/src/App.js
+++ flavio-js/github-api-react/react-github-users/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Form from './components/form.js'
+import React, {useState} from "react"
+import CardList from './components/CardList';
 
 function App() {
+  const [cards, setCards] = useState([])
+
+  const addNewCard = (card) =>{
+    setCards ([...cards, card])
+  }
   return (
     <div>
       <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
         Search GitHub User</h1>
-            <Form/>
+            <Form onSubmit={addNewCard}/>
+            <CardList cards={cards} />
     </div>
   )
 }
