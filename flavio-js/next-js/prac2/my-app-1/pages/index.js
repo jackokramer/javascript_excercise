c7f67035960fca014171f8b965295f682c5d80b7
jackokramer <jackokramer@gmail.com>
2021-06-30 12:00:10 -0700

    js_clock

---
+++ flavio-js/next-js/prac2/my-app-1/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Index = () => (
+  <div>
+    <h1>Home page</h1>
+  </div>
+)
+
+
+export default Index
