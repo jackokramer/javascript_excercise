e9880b2d0a61de32f348eff6bef0c37d12210d5d
jackokramer <jackokramer@gmail.com>
2021-08-01 14:32:33 -0700

    removed custom.js

--- custom.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function(){
-    if(document.querySelectorAll('[name="checkout[reduction_code]"]').length > 0)
-   {  dataLayer.push({'event':'promo','code':document.querySelector('[name="checkout[reuction_code]"]').value});
-    }
-    },2000);
