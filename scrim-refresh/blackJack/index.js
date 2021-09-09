5a7f462e8e1881b7c2b25822cde63eb7c870ff57
jackokramer <jackokramer@gmail.com>
2021-09-08 21:13:40 -0700

    complete black jack game with unique style layouts

--- scrim-refresh/blackJack/index.js
+++ scrim-refresh/blackJack/index.js
@@ -1,41 +1,83 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ''
-
 let messageEl = document.getElementById('message-el')
-// let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector('#sum-el')
-let cardsEl = document.querySelector('#cards-el')
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.getElementById('cards-el')
+
+let playerInfo = {
+    name: 'jack',
+    value: 290,
+    sayHi: function(){
+        console.log('Hi')
+    }
+
+}
+let playerEl = document.getElementById('player-el')
+playerEl.textContent = playerInfo.name + " : " + playerInfo.value
+
+// Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard(){
+        // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumber =  Math.floor(Math.random()*13)+1
+    if(randomNumber>10){
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame(){
-if(sum <= 20){
+    isAlive = true
+     // Generate two random numbers
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+     // Re-assign the cards and sum variables so that the game can start
+     cards= [firstCard, secondCard]
+     sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for(let x = 0; x< cards.length; x++){
+        cardsEl.textContent += cards[x] + " "
+    }
+    //cardsEl.textContent = 'Cards are : ' +  cards[0] + " and " + cards[1]
+    //render out all the cards we have
+    if(sum <= 20){
+    //render out first card and second card
+    // Create a for loop that renders out all the cards instead of just two
     sumEl.textContent= 'Sum: ' + sum
-    cardsEl.textContent = 'Cards are : ' +  firstCard + " and " + secondCard
     message = `would you like another card?`
 } else if(sum ===21){
     hasBlackJack = true
     sumEl.textContent = 'Sum: ' + sum
-    cardsEl.textContent =  'Cards are : ' + firstCard  + " and " + secondCard
     message = `nailed it baby black jack!!`
 } else {
-    sumEl.textContent = 'Sum: ' + sum
-    cardsEl.textContent = 'Cards are : ' + firstCard + " and " +secondCard
     isAlive = false
     message = `later loser`
 }
-    messageEl.textContent = message
+messageEl.textContent = message
 console.log(message)
 }
 
 //New Card function
 function newCard(){
-    console.log('Drawing a new card from the deck')
-    let card = 7
-    sum = card + firstCard + secondCard
-    startGame()
+        // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
 }
 
 /*
@@ -76,5 +118,51 @@ if (age < 100) {
     console.log(`Here's your birthday card from the King!!!!!👑`)
 } else{
     console.log(`not elegible already got one`)
+}
+*/
+/*
+let featuredLists = ["check out my project", "here's the code for my project", 'and my portfolio']
+
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let myList = ['Univeristy of Oregon', 'Professional Surfer', 'Avid reader', 'Student of the arts']
+*/
+
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+//let mySelf = ['Jack', 17, false]
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+
+// Push the newMessage to the messages array, and then log out the array
+/*
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+
+console.log(messages)
+*/
+
+
+// Count to ten!
+
+// We need to specify...
+
+// Where should we START counting?
+// Where is the FINISH line?
+// What's the STEP SIZE we should use?
+/*
+//    START           FINISH       STEP SIZE
+for ( let count = 1;  count < 11;  count += 1 )  {
+
+    console.log(count)
+
 }
 */
