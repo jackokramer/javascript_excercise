5e65dafdd6f7cf30c13517e6a030e9a6302459ba
jackokramer <jackokramer@gmail.com>
2021-09-10 13:53:48 -0700

    added delete button and now adding an save tab functionality

--- scrim-refresh/linkedin-app/index.js
+++ scrim-refresh/linkedin-app/index.js
@@ -1,24 +1,43 @@
+//if an array has backticks `` use JSON.stringify() to allow push function to add others to the array
+//myLeads = Json.parse(myLeads) will turn a set of strings into an array
+//myLeads = JSON.stringify(myLeads) turns them all into strings.
 let myLeads = []
+let oldLeads = []
+console.log(myLeads)
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
-// 2. Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById('ul-el')
 console.log(ulEl)
-inputBtn.addEventListener('click', function(){
-   myLeads.push(inputEl.value)
-   console.log(myLeads)
-   renderLeads()
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn= document.getElementById('tab-btn')
+
+const leadsFromLocal = JSON.parse(localStorage.getItem('myLeads'))
+console.log(leadsFromLocal)
+
+if(leadsFromLocal){
+    myLeads = leadsFromLocal
+    render(myLeads)
+}
+
+const tabs = [
+    {
+        url:"https://www.linkedin.com/in/per-harald-borgen/"
+    }
+]
+
+tabBtn.addEventListener('click', function(){
+    console.log(tabs[0])
 })
 
 // render out the lead
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for(let x = 0; myLeads.length>x ; x++){
+    for(let x = 0; leads.length>x ; x++){
         // Wrap the lead in an anchor tag (<a>) inside the <li>
         // Can you make the link open in a new tab?
         listItems += `<li>
-                        <a target='_blank' href='${myLeads[x]}'>
-                         ${myLeads[x]} </a>
+                        <a target='_blank' href='${leads[x]}'>
+                         ${leads[x]} </a>
                     </li>`
         console.log(listItems)
         //create element
@@ -32,3 +51,25 @@ function renderLeads(){
         inputEl.value = " "
         //listItems.window.open()
 }
+
+// 2. Listen for double clicks on the delete button (google it!)
+deleteBtn.addEventListener('dblclick', function(){
+    console.log('double stuffed')
+    localStorage.clear(myLeads)
+    myLeads = []
+    render(myLeads)
+    //deleteBtn.classList.toggle('myLeads')
+})
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+
+inputBtn.addEventListener('click', function(){
+    myLeads.push(inputEl.value)
+    localStorage.setItem('myLeads',
+    JSON.stringify(myLeads))
+   render(myLeads)
+   console.log(localStorage.getItem('myLeads'))
+})
+
+
+/* arguments vs parameters
+*/
