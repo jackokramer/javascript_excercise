fb6b6308753738b5a1e3f18dbdd9f8a03c42b4d2
jackokramer <jackokramer@gmail.com>
2021-09-22 19:52:30 -0700

    complete snake game via scrim tutorial

--- scrim-refresh/snake_game/index.js
+++ scrim-refresh/snake_game/index.js
@@ -6,10 +6,16 @@ let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
 let width = 10
+let appleIndex = 0
+let score = 0
+let speed = 0.9
+let intervalTime = 1000
+let timerId = 0
+
 
 function createGrid(){
     //create 100 of these elements with a for loop
-    for(let x = 0; x< 100; x++){
+    for(let x = 0; x< width*width; x++){
     const square = document.createElement('div')
         // create element
     square.classList.add('square')
@@ -24,12 +30,31 @@ createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
+function startGame(){
+    //remove the snake for the grid and apple
+    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    squares[appleIndex].classList.remove('apple')
+    //clearInterval(timerId)
+    currentSnake = [2,1,0]
+    //generate apple -done
+    score =0
+    // reapply score to the browser
+    scoreBoard.textContent = score
+
+    direction = 1
+    intervalTime = 1000
+    generateApple()
+    //read the class of snake to our current Snake
+    currentSnake.forEach(index => squares[index].classList.add('snake'))
+    const timerId = setInterval(moveSnake, 1000)
+}
+
 function moveSnake (){
     if(
-        (currentSnake[0] + width >= 100 && direction === 10) ||// if snake has hit bottom
-        (currentSnake[0] % width === 9 && direction ===1) ||// if snake has hit right
+        (currentSnake[0] + width >= width*width && direction === width) ||// if snake has hit bottom
+        (currentSnake[0] % width === width-1 && direction ===1) ||// if snake has hit right
         (currentSnake[0] % width === 0 && width ===-1) ||// if snake has hit left
-        (currentSnake[0] -  width< 0 && direction ===-10)||// if snake has hit top
+        (currentSnake[0] -  width< 0 && direction === -width)||// if snake has hit top
         squares[currentSnake[0]+direction].classList.contains('snake')
     )
     return clearInterval(timerId)
@@ -44,11 +69,45 @@ function moveSnake (){
     lastEl.pop(squares).style.display = 'none' */
     //unshift() adds an element to the front of an arrays
     currentSnake.unshift(currentSnake[0]+direction)
+
+        /// deal with snake head getting the apple
+        if(squares[currentSnake[0]].classList.contains('apple')) {
+            //remove the (class) apple
+            squares[currentSnake[0]].classList.remove('apple')
+            //grow snake by one by adding class of snake
+            squares[tail].classList.add('snake')
+            console.log(tail)
+            // grow our snake array
+            currentSnake.push(tail)
+
+            // generate a new apple
+            generateApple()
+
+            // add one to the score
+            score += 1
+            scoreBoard.textContent = score
+            // speed up the snake
+            clearInterval(timerId)
+            console.log(intervalTime)
+            intervalTime * speed
+            console.log(intervalTime)
+            timerId = setInterval(moveSnake, intervalTime)
+        }
+
+
+
     squares[currentSnake[0]].classList.add('snake')
 }
-moveSnake()
 
-const timerId = setInterval(moveSnake, 1000)
+
+function generateApple(){
+    do {
+        // generate a random number
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+        squares[appleIndex].classList.add('apple')
+}
+generateApple()
 //clearInterval(timerId)
 
 //keycode 39 = right arrow
@@ -72,5 +131,6 @@ function control(e){
     }
 }
 document.addEventListener('keyup', control)
-
-control()
+startBtn.addEventListener('click',function(){
+    startGame()
+})
