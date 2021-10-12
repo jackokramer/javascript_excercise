5e5abb9b8c6c222acb79766618fb9b960765b081
jackokramer <jackokramer@gmail.com>
2021-10-11 23:18:09 -0700

    currated blog post

---
+++ scrim-refresh/blog-post/index.js
@@ -0,0 +1,39 @@
+/**
+ Challenge:
+
+ GET a list of blog posts from the JSON Placeholder API.
+
+ BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ Endpoint: /posts
+
+ Since there's so many posts, let's limit the array to just 5 items.
+ You can use the `.slice()` array method to just grab the first 5 objects
+ from the data array that comes back from the API
+
+ Log the 5 items to the console
+ */
+
+ /**
+ Challenge:
+
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
+
+ Hints:
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you
+   can then put into the div with `innerHTML`
+ */
+
+ fetch('https://apis.scrimba.com/jsonplaceholder/posts', {method: 'GET'})
+    .then(response => response.json())
+    .then(data =>  {
+        const postArr = data.slice(0,5)
+        console.log(postArr)
+        let html = ''
+        for(let post of postArr) {
+            html += `<h3>${post.title}</h3>
+            <p>${post.body}</p>`
+        }
+        document.getElementById('items').innerHTML = html
+        })
