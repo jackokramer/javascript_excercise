4543a10d76b757b9d38b832a6709b789bbc0300a
jackokramer <jackokramer@gmail.com>
2022-02-21 19:46:20 -0800

    graphql info

--- capital-valen/index.js
+++ capital-valen/index.js
@@ -12,5 +12,5 @@ function correct() {
     // Write the JavaScript to grab the message from the paragraph and correct its capitalisation.
     // It should read "Valentine's"
     // Render the corrected message to the DOM.
-
+//
 }
