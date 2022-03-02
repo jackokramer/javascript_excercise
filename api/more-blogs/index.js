83f26678460f7b3a7738457832d52ff7d718e451
jackokramer <jackokramer@gmail.com>
2022-03-01 23:58:24 -0800

    more styling for the api blog

--- api/more-blogs/index.js
+++ api/more-blogs/index.js
@@ -1,9 +1,8 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data =>{
-        const postArrBlog = data.slice(0, 5)
-        let html = ""
-        for(let post of postArrBlog){
+let postArrayBlog= []
+
+function renderPosts(){
+    let html = ""
+        for(let post of postArrayBlog){
             html += `
             <h3>${post.title}</h3>
                 <p>${post.body}</p>
@@ -11,18 +10,37 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
             `
         }
         document.getElementById('blog-space').innerHTML = html
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data =>{
+        postArrayBlog = data.slice(0, 5)
+        renderPosts()
     })
 
-document.getElementById('new-post').addEventListener('submit', (event) =>{
-    event.preventDefault()
+document.getElementById('new-post').addEventListener('submit', (e) =>{
+    e.preventDefault()
     let submittedTitle = document.getElementById('post-title').value
     let submittedText = document.getElementById('post-body').value
     console.log(submittedText)
     const data ={
         title: submittedTitle,
         body: submittedText
-
     }
-          //console.log(submittedInfo)
-
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => {
+        //console.log(data)
+       postArrayBlog.unshift(data)
+       renderPosts()
+        })
+         document.getElementById("new-post").reset();
 })
---
+++ api/more-blogs/toggle.js
@@ -0,0 +1,7 @@
+const toggleSwitch = document.querySelector(".nav-toggle")
+
+
+
+toggleSwitch.addEventListener("click", ()=>{
+    document.body.classList.toggle('nav-open')
+})
