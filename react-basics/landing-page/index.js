cf97eab8e9a530a2de0699d748762c9d88fff147
jackokramer <jackokramer@gmail.com>
2022-03-08 23:45:00 -0800

    sleep

--- react-basics/landing-page/index.js
+++ react-basics/landing-page/index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h2>More react facts</h2>, document.getElementById('root'))
+const Parent = (
+    <div>
+        <img src="./react-logo.png" width="40px"/>
+            <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was realeased in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powered by thousands of Enterprise apps, including mobile apps</li>
+                </ul>
+    </div>
+)
+
+
+ReactDOM.render(page, document.getElementById("root"))
+
