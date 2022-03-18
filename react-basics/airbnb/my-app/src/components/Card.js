e12b646185f75f8fb8d710a4f5c9e556932fec00
jackokramer <jackokramer@gmail.com>
2022-03-17 17:58:01 -0700

    airbnb clone

--- react-basics/airbnb/my-app/src/components/Card.js
+++ react-basics/airbnb/my-app/src/components/Card.js
@@ -1,20 +1,26 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
+        let badgeText
+        if(props.openSpots === 0){
+            badgeText ="Sold Out"
+        } else if(props.location === 'Online'){
+            badgeText ="Online"
+        }
     return (
         <div className="card">
-            <h1>Replace this</h1>
-                <img src="../images/katie-zaferes.png" className="card-photo"/>
+            {badgeText&&<div className="card-badge">{badgeText}</div>}
+                <img src={`../images/${props.img}`} className="card-photo"/>
                 <br/>
                 <div className="card-info">
                     <span>
                         <img src="../images/star.png" className="card-star"/>
                     </span>
-                    <span className="grey">5.0</span>
-                <span className="grey">(6) - USA</span>
+                    <span className="grey">{props.rating}</span>
+                <span className="grey">({props.reviewCount}) - USA</span>
                 </div>
-                 <p className="card-more-info"> Life lessons with Katie</p>
-                <p className="card-price"><span className="card-italic-price">From $138</span>/per person</p>
+                 <p className="card-more-info">{props.title}</p>
+                <p className="card-price"><span className="card-italic-price">From ${props.price}</span>/per person</p>
 
         </div>
 
