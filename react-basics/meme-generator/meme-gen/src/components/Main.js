f022995c7d1fc63eda22c8372b33fe06deb18000
jackokramer <jackokramer@gmail.com>
2022-03-26 15:50:03 -0700

    sleeper hit

--- react-basics/meme-generator/meme-gen/src/components/Main.js
+++ react-basics/meme-generator/meme-gen/src/components/Main.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import memesData from '../memesData'
 export default function Main(){
-        const [memeImg, setMemeImg] = React.useState('')
+    const [meme, setMeme] = React.useState(
+        {
+            topText: '',
+            bottomText: '',
+            randomImg: 'http://i.imgflip.com/1bij.jpg'
+        })
+    const [allMemeImg, setAllMemeImg] = React.useState(memesData)
 
     function memeData (){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImg.data.memes
         const randomNumber = Math.floor(Math.random()*memesArray.length)
-        setMemeImg(memesArray[randomNumber].url)
+        const url= memesArray[randomNumber].url
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            randomImg: url
+        }))
         //const url = memesArray[randomNumber].url
         //console.log(url)
 
@@ -21,7 +31,7 @@ export default function Main(){
                  placeholder="bottom-text"/>
                     <button className="btn btn-form" onClick={memeData}>Generate New Meme 🏞</button>
             </div>
-            <img src={memeImg} className="meme-img"/>
+            <img src={meme.randomImg} className="meme-img"/>
         </div>
     )
 }
