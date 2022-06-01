f083432e4fbf5621b1d356c62ff99ddc143356ba
jackokramer <jackokramer@gmail.com>
2022-05-31 23:15:48 -0700

    another modal

---
+++ modal/index.js
@@ -0,0 +1,8 @@
+
+ document.getElementById('contact-me').addEventListener('click', () =>{
+    document.getElementById('overlay').style.display = 'block'
+})
+
+document.getElementById('close-modal').addEventListener('click', () =>{
+    document.getElementById('overlay').style.display = 'none'
+})
