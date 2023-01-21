b0f39192148217aa0c9bba0db001839d230188d6
jackokramer <jackokramer@gmail.com>
2023-01-20 23:21:07 -0800

    cap algo

---
+++ algos/cap-algo.js
@@ -0,0 +1,9 @@
+// Write a function that capitalizes a string
+let msg = "hello";
+
+function cap(str){
+    return str.toUpperCase();
+}
+
+console.log(cap(msg)) // HELLO 
+console.log(cap("scrimba")) // SCRIMBA
