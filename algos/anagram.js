3307c4d9b0aa719ef24b38c31184c7b0a5a77555
jackokramer <jackokramer@gmail.com>
2023-01-30 21:09:38 -0800

    more algos

--- algos/anagram.js
+++ algos/anagram.js
@@ -1,21 +1,12 @@
 //Given two strings ‘a’ and string ‘b’, we have to check if they are anagrams of each other or not and return True/False. For example,
 
 function anagramCheck(a, b){
-    let lenOne = a.length
-    let lenTwo = b.length
-    if(lenOne !== lenTwo){
-        console.log('Invalid Input')
-        return
-    }
-    let word1 = a.split(' ').sort().join(' ')
-    let word2 = b.split(' ').sort().join(' ')
-    if(word1 === word2) {
-        console.log('True')
-    } else {
-        console.log('false')
-    }
+    if(a.length !== b.length)return false
+    const word1 = a.split('').sort().join('')
+    const word2 = b.split('').sort().join('')
+    return word1 === word2
 }
 
-//console.log(anagramCheck('booth', 'tooth'))
+// console.log(anagramCheck('booth', 'tooth'))
 
 console.log(anagramCheck('camry', 'mycar'))
