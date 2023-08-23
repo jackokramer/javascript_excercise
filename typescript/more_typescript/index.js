eaa9c4bb71bbcfca530fe2de355743ec747666e2
jackokramer <jackokramer@gmail.com>
2023-08-22 23:41:45 -0700

    new typescript module

---
+++ typescript/more_typescript/index.js
@@ -0,0 +1,11 @@
+let foo
+foo = 3
+console.log(foo, `line 3`)
+foo = `cat`
+
+const addition=(x,y)=>{
+    return x +y
+}
+console.log(addition(3,5))
+console.log(addition(3,'fires'))
+console.log(foo, `line 5`)
