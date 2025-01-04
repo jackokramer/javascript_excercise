da857865d8f564b07780aa968ca930745fbf8118
jackokramer <jackokramer@gmail.com>
2025-01-03 23:26:56 -0800

    more blackjack

--- black_jack/index.js
+++ black_jack/index.js
@@ -1,6 +1,7 @@
 let cardOne = 11
 let cardTwo = 10
 let sum = cardOne+cardTwo
+let hasBlackJack = false
 
 let message = ``
 
@@ -8,11 +9,14 @@ let messageEl = document.getElementById("message-el")
 
 function drawCard(){
     if(sum <=20){
+        hasBlackJack = false
         message = `You have ${sum} pleasse draw more`
     } else if(sum===21){
         message = "Black jack baby"
+        hasBlackJack = true
     } else {
         message = `You lose loswe ${sum}`
+        hasBlackJack = false
     }
     messageEl,textContent= message
 }
