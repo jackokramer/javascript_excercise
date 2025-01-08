dd9f0ebdaf413db0a0507cf55e89a77b955b4283
jackokramer <jackokramer@gmail.com>
2025-01-07 18:51:13 -0800

    fixed js bug

--- black_jack/index.js
+++ black_jack/index.js
@@ -6,16 +6,23 @@ let isAlive = true
 let message = ` `
 
 let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
 
 function startGame(){
+    sumEl.textContent= sum
     if(sum <=20){
-        message = `You have ${sum} pleasse draw more`
-    } else if(sum===21){
+        console.log(`do you want to draw a new card?`)
+        message = `You have pleasse draw more`
+    } else if(sum === 21){
+        console.log(`You Win!!!`)
         message = "Black jack baby"
         hasBlackJack = true
     } else {
-        message = `You lose loswe ${sum}`
+        console.log(`You lose.....`)
+        message = `You lose loser`
         isAlive=false
     }
     messageEl.textContent= message
 }
+
+console.log(sum)
