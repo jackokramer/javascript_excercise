2aa9bd571d6f2f9585f94e1fcb5b16d30f9f68cd
jackokramer <jackokramer@gmail.com>
2026-04-14 11:28:50 -0700

    daily weather card build 10 minutes

--- chrome_extend/index.js
+++ chrome_extend/index.js
@@ -8,18 +8,23 @@ const ulEl = document.getElementById('ul-el')
 localStorage.clear()
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"))
 
-
-
 if(leadsFromLocalStorage){
     myLeads= leadsFromLocalStorage
-    renderLeads()
+    render()
 }
+function render(){
+    let listItem = `<li>
+                    <a href="${inputEl.value}" target="_blank">`+ inputEl.value + `</a>
+                </li>`
+    ulEl.innerHTML+=listItem
+} //more efficient as it doens't loop through everything
+
 
 // delete button
 deleteBtn.addEventListener('dblclick', ()=>{
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render()
 })
 
 //input button
@@ -27,30 +32,7 @@ inputBtn.addEventListener('click',()=>{
     myLeads.push(inputEl.value)
     console.log(myLeads)
     localStorage.setItem('leads', JSON.stringify(myLeads))
-    renderLeads()
+    render()
     console.log( localStorage.getItem("leads") )
     inputEl.value = ' '
 })
-
-
-function renderLeads(){
-    let listItem = `<li>
-                    <a href="${inputEl.value}" target="_blank">`+ inputEl.value + `</a> </li>`
-    ulEl.innerHTML+=listItem
-} //more efficient as it doens't loop through everything
-
-
-
-
-
-
-// function renderLeads(){
-// let listItems =''
-// for(let x=0; x<myLeads.length; x++){
-//     listItems += `<li>${myLeads[x]}</li> `
-//     console.log(listItems)
-
-// }
-
-// ulEl.innerHTML += listItems
-// }
