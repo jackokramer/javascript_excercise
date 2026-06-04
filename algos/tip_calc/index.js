946e553e2c9765d710d22d54830c38e6ac04c25a
jackokramer <jackokramer@gmail.com>
2026-06-04 13:14:49 -0700

    tip amountcalculator

---
+++ algos/tip_calc/index.js
@@ -0,0 +1,23 @@
+// generate a random number for
+// amount anywhere from $20 to $150
+// have three different buttons for percenetage
+let amount = document.getElementById('amount')
+
+function generateRand(){
+    amount = Math.random()
+}
+
+15%
+function fifteen(){
+
+}
+
+// 18%
+function eighteen(){
+
+}
+
+// 20%
+function twenty(){
+
+}
